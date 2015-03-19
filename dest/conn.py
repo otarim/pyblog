4e62c9f536c4c9dbd5057c134023fdbd82a99f41
otarim <otarim.com@gmail.com>
2015-03-19 18:10:26 +0800

    css

--- dest/conn.py
+++
@@ -1,4 +0,0 @@
-from pymongo import MongoClient
-
-client = MongoClient('localhost', 27017)
-# client.admin.authenticate("vps309","vps309!")
--- dest/sign.py
+++
@@ -1,7 +0,0 @@
-import hashlib
-
-def sign(username):
-	s = hashlib.md5()
-	s.update('_' + str(id))
-	s.update(hashlib.sha1('_misaka').digest())
-	return s.hexdigest()[:13]
